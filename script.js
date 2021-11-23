2d4af0f9be2a7eb7c9afa3135743a18a9f187f2a
Lucs Lisboa <lucas@pop-os.localdomain>
2021-11-23 15:16:03 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const botaoInserir = document.getElementById('criar-tarefa');
 function criarLi() {
     const elementoLi = document.createElement('li');
     elementoLi.innerText = document.getElementById('texto-tarefa').value;
+    elementoLi.className = 'liDinamico'
     listaOl.appendChild(elementoLi);
     document.getElementById('texto-tarefa').value = ''
 }
 
 botaoInserir.addEventListener('click', criarLi);
+
+function selecionaLi(alvo) {
+    if (document.getElementsByClassName('liSelecionado')[0]){
+        document.getElementsByClassName('liSelecionado')[0].className = 'liDinamico'
+    } if (alvo.target.classList[0] === 'liDinamico') {
+        alvo.target.className = 'liDinamico liSelecionado'
+    }
+}
+
+listaOl.addEventListener('click', selecionaLi);
