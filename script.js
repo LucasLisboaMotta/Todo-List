d905fd54ef8b94dafd2d702bf4c47423c94b5f3e
Lucs Lisboa <lucas@pop-os.localdomain>
2021-11-23 15:50:05 -0300

    requisito 9

--- script.js
+++ script.js
@@ -2,21 +2,34 @@ const listaOl = document.getElementById('lista-tarefas');
 const botaoInserir = document.getElementById('criar-tarefa');
 
 function criarLi() {
-    const elementoLi = document.createElement('li');
-    elementoLi.innerText = document.getElementById('texto-tarefa').value;
-    elementoLi.className = 'liDinamico'
-    listaOl.appendChild(elementoLi);
-    document.getElementById('texto-tarefa').value = ''
+  const elementoLi = document.createElement('li');
+  elementoLi.innerText = document.getElementById('texto-tarefa').value;
+  elementoLi.className = 'liDinamico';
+  listaOl.appendChild(elementoLi);
+  document.getElementById('texto-tarefa').value = '';
 }
 
 botaoInserir.addEventListener('click', criarLi);
 
 function selecionaLi(alvo) {
-    if (document.getElementsByClassName('liSelecionado')[0]){
-        document.getElementsByClassName('liSelecionado')[0].className = 'liDinamico'
-    } if (alvo.target.classList[0] === 'liDinamico') {
-        alvo.target.className = 'liDinamico liSelecionado'
-    }
+  if (document.getElementsByClassName('liSelecionado')[0]) {
+    document.getElementsByClassName('liSelecionado')[0].classList.remove('liSelecionado');
+  } if (alvo.target.classList[0] === 'liDinamico') {
+    const selecionaAlvo = alvo.target;
+    selecionaAlvo.classList.add('liSelecionado');
+  }
 }
 
 listaOl.addEventListener('click', selecionaLi);
+
+function cortaLi(alvo) {
+  const selecionado = alvo.target;
+  const selecionadoArray = selecionado.classList;
+  if (selecionadoArray[0] === 'liDinamico' && selecionadoArray.contains('completed')) {
+    selecionado.classList.remove('completed');
+  } else if (selecionadoArray[0] === 'liDinamico') {
+    selecionado.classList.add('completed');
+  }
+}
+
+listaOl.addEventListener('dblclick', cortaLi);
